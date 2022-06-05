51532764d2e255a1733007316320b5eb559d4d85
Aiden-Yoo <you1367@gmail.com>
2022-06-05 13:18:18 +0900

    Session with iron-session

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,13 +1,28 @@
+import { withIronSessionApiRoute } from "iron-session/next";
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
   const { token } = req.body;
-  console.log(token);
+  const exists = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+  });
+  if (!exists) res.status(404).end();
+  req.session.user = {
+    id: exists?.userId,
+  };
+  await req.session.save();
   res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+  cookieName: "carrotsession",
+  password:
+    "123qwe2213123qqwe123qwe2213123qqwe123qwe2213123qqwe123qwe2213123qqwe123qwe2213123qqwe",
+});
