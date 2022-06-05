cb748045e6edf08105c79c80fbb1f2d08a52c0ea
Aiden-Yoo <you1367@gmail.com>
2022-06-05 16:37:57 +0900

    Profile handling with API Session

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,28 +1,29 @@
-import { withIronSessionApiRoute } from "iron-session/next";
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
   const { token } = req.body;
-  const exists = await client.token.findUnique({
+  const foundToken = await client.token.findUnique({
     where: {
       payload: token,
     },
   });
-  if (!exists) res.status(404).end();
+  if (!foundToken) return res.status(404).end();
   req.session.user = {
-    id: exists?.userId,
+    id: foundToken.userId,
   };
   await req.session.save();
-  res.status(200).end();
+  await client.token.deleteMany({
+    where: {
+      userId: foundToken.userId,
+    },
+  });
+  res.json({ ok: true });
 }
 
-export default withIronSessionApiRoute(withHandler("POST", handler), {
-  cookieName: "carrotsession",
-  password:
-    "123qwe2213123qqwe123qwe2213123qqwe123qwe2213123qqwe123qwe2213123qqwe123qwe2213123qqwe",
-});
+export default withApiSession(withHandler("POST", handler));
